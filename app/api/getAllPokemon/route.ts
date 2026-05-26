521e7d32e4555087e73d7823bd16966f0032ebd5
김혜진 <khj000217@naver.com>
2026-05-26 11:32:18 +0900

    fix: attack = attack+speical_attack 합치기

--- app/api/getAllPokemon/route.ts
+++ app/api/getAllPokemon/route.ts
@@ -4,7 +4,28 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const client = await getClientPromise();
   const pokemonColl = client.db('pokemon').collection('pokemon');
-  const pokemon = await pokemonColl.find({}).toArray();
 
-  return NextResponse.json({ ok: true, result: pokemon });
+  const result = await pokemonColl.updateMany({}, [
+    {
+      $set: {
+        'baseStats.attack': {
+          $add: [
+            '$baseStats.attack',
+            { $getField: { field: 'special-attack', input: '$baseStats' } },
+          ],
+        },
+        'baseStats.defense': {
+          $add: [
+            '$baseStats.defense',
+            { $getField: { field: 'special-defense', input: '$baseStats' } },
+          ],
+        },
+      },
+    },
+  ]);
+
+  return NextResponse.json({ ok: true, result });
+  // const pokemon = await pokemonColl.find({}).toArray();
+
+  // return NextResponse.json({ ok: true, result: pokemon });
 }
