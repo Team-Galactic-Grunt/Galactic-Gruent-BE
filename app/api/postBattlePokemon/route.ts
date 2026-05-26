6b893a61dc33ad96af6bd80ede21e91ba094ec69
김혜진 <khj000217@naver.com>
2026-05-26 09:20:45 +0900

    feat: getAllPokemon 가져오기

--- app/api/postBattlePokemon/route.ts
+++ app/api/postBattlePokemon/route.ts
@@ -1,7 +1,8 @@
-import { getClientPromise } from '@/app/lib/mongodb';
+import { getClientPromise } from '@/app/lib/mongodb'; // ◀ 중괄호 { } 추가
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function POST(req: Request) {
+  // getClientPromise가 async 함수이므로 await를 붙여서 client 인스턴스를 가져옵니다.
   const client = await getClientPromise();
   const pokemonColl = client.db('pokemon').collection('battle_pokemon');
   const pokemon = await pokemonColl.find({}).toArray();
