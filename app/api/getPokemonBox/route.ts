ceda1607541b0caa672884641bc2e3d6e147acfc
김혜진 <khj000217@naver.com>
2026-05-26 17:42:39 +0900

    docs: 포켓몬 박스 추가

--- app/api/getPokemonBox/route.ts
+++ app/api/getPokemonBox/route.ts
@@ -6,8 +6,43 @@ const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 export async function GET() {
   const client = await getClientPromise();
 
-  const pokemonColl = client.db('pokemon').collection('pokemon');
   const boxColl = client.db('pokemon').collection('pokemon_box');
+  const historyColl = client.db('pokemon').collection('game_history');
+  // 현재 파티 포켓몬 ID 목록
+  const history = await historyColl.findOne(
+    {},
+    {
+      projection: {
+        _id: 0,
+        pokemon: 1,
+      },
+    },
+  );
 
-  return NextResponse.json({ ok: true, message: '초기 데이터 API' });
+  const myPokemonSet = new Set(history?.pokemon ?? []);
+
+  // 전체 포켓몬
+  const allPokemon = await boxColl.find({}).toArray();
+
+  // 현재 소지 포켓몬
+  const isMyPokemon = allPokemon.filter((pokemon) =>
+    myPokemonSet.has(pokemon.catchId),
+  );
+
+  // 박스 포켓몬
+  const pokemonBox = allPokemon.filter(
+    (pokemon) => !myPokemonSet.has(pokemon.catchId),
+  );
+
+  return NextResponse.json({
+    ok: true,
+
+    message: '포켓몬 박스 조회',
+
+    data: {
+      isMyPokemon,
+
+      pokemonBox,
+    },
+  });
 }
