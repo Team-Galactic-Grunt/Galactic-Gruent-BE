ac75879acc3a314997b16cb18298119252809c7c
김혜진 <khj000217@naver.com>
2026-05-26 10:19:23 +0900

    fix: 연결 collection 수정

--- app/api/getAllPokemon/route.ts
+++ app/api/getAllPokemon/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const client = await getClientPromise();
-  const pokemonColl = client.db('pokemon').collection('battle_pokemon');
+  const pokemonColl = client.db('pokemon').collection('pokemon');
   const pokemon = await pokemonColl.find({}).toArray();
 
   return NextResponse.json({ ok: true, result: pokemon });
