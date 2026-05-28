e97b60d1086adeddeedd1d93ee0e32f4d6207970
김혜진 <khj000217@naver.com>
2026-05-28 11:06:07 +0900

    feat: 관리자 페이지 가방 get api 수정

--- app/api/changeItemsApi/route.ts
+++ app/api/changeItemsApi/route.ts
@@ -2,6 +2,40 @@ import { getClientPromise } from '@/app/lib/mongodb';
 import { NextResponse } from 'next/server';
 import { v4 as uuidv4 } from 'uuid';
 
+export async function GET(req: Request) {
+  const body = await req.json();
+  const { bag } = body;
+
+  console.log(`bag: ${JSON.stringify(bag)}`);
+
+  const client = await getClientPromise();
+  const db = client.db('pokemon');
+  const historyColl = db.collection('game_history');
+
+  const result = await historyColl.findOne(
+    {},
+    {
+      projection: {
+        _id: 0,
+        bag: 1,
+      },
+    },
+  );
+
+  if (result) {
+    return NextResponse.json({
+      ok: true,
+      message: '갯수 반영 성공',
+      data: result,
+    });
+  }
+
+  return NextResponse.json({
+    ok: false,
+    message: '갯수 반영 실패',
+  });
+}
+
 export async function POST(req: Request) {
   const body = await req.json();
   const { bag } = body;
--- app/api/postReport/route.ts
+++ app/api/postReport/route.ts
@@ -1,6 +1,30 @@
 import { getClientPromise } from '@/app/lib/mongodb'; // ◀ 중괄호 { } 추가
 import { NextResponse } from 'next/server';
 
+export async function GET() {
+  const client = await getClientPromise();
+  const db = client.db('pokemon');
+  const historyColl = db.collection('game_history');
+
+  const result = await historyColl.findOne(
+    {},
+    { projection: { _id: 0, bag: 1 } },
+  );
+
+  if (result) {
+    return NextResponse.json({
+      ok: true,
+      message: '가방 조회 성공',
+      data: result,
+    });
+  }
+
+  return NextResponse.json({
+    ok: false,
+    message: '가방 조회 실패',
+  });
+}
+
 export async function POST(req: Request) {
   const body = await req.json();
   const { position, bag, isMyPokemon, pokemonBox } = body;
